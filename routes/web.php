f95e9024f6586b25cb4097a031920d80a5015a0c
Safutra-FG <safutrafadlig1@gmail.com>
2025-11-10 10:55:27 +0700

    PBO aduhay

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,6 @@ use App\Http\Controllers\c_contact;
 use App\Http\Controllers\c_mahasiswa;
 
 
-Route :: view('/', function () {
-    return view('welcome2');
-});
-
 
 Route :: view('/', 'v_dashboard');
 Route :: get('/home', [c_home::class,'index']);
